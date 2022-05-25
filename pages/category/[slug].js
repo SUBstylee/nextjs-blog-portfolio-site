7fc2ea13911bc46fed740d429ab40d2dca5c9bdf
SUBstylee <jjthrelfall@gmail.com>
2022-05-25 23:25:46 +0800

    changed classname

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -23,7 +23,7 @@ const CategoryPost = ({ posts }) => {
 
     return (
         <div className="container mx-auto px-10 mb-8">
-            <div className="font-bold text-2xl text-white mb-5 mt-1"><h2>{`Posts From ${cat}`}</h2></div>
+            <div className="font-bold text-2xl text__theme mb-5 mt-1"><h2>{`Posts From ${cat}`}</h2></div>
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                 <div className="col-span-1 lg:col-span-8">
                     {posts.map((post, index) => (
