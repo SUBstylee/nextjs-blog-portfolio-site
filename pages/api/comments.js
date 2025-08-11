9bea97ad631d6c36c479366ec34716c8202b5226
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-11 23:48:44 +0800

    was having an issue where comments were hitting a 404 error. had to adjust settings in hygraph, but was testing things locally at first, so decided to keep the error logging

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,22 +1,48 @@
 //any file inside the folder pages/api is mapped to /api/* and will be treated as an API endpoint instead of a page
 
-import { GraphQLClient, gql } from "graphql-request";
+import { GraphQLClient, gql } from 'graphql-request'
 
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
-const graphcmsToken = process.env.GRAPHCMS_TOKEN;
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+const graphcmsToken = process.env.GRAPHCMS_TOKEN
 
 export default async function comments(req, res) {
   const graphQLClient = new GraphQLClient(graphqlAPI, {
     headers: {
-      authorization: `Bearer ${graphcmsToken}`
-    }
-  });
+      authorization: `Bearer ${graphcmsToken}`,
+    },
+  })
+
   const query = gql`
-    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!){
-      createComment(data: {name:$name,email:$email,comment:$comment,post:{connect:{slug:$slug}}}){id}
+    mutation CreateComment(
+      $name: String!
+      $email: String!
+      $comment: String!
+      $slug: String!
+    ) {
+      createComment(
+        data: {
+          name: $name
+          email: $email
+          comment: $comment
+          post: { connect: { slug: $slug } }
+        }
+      ) {
+        id
+      }
     }
   `
 
-  const result = await graphQLClient.request(query, req.body);
-  return res.status(200).send(result);
-};
+  try {
+    const result = await graphQLClient.request(query, req.body)
+    return res.status(200).json(result)
+  } catch (error) {
+    console.error('Error creating comment:', JSON.stringify(error, null, 2))
+    if (error.response && error.response.errors) {
+      console.error(
+        'GraphQL errors:',
+        JSON.stringify(error.response.errors, null, 2)
+      )
+    }
+    return res.status(500).json({ error: 'Failed to create comment' })
+  }
+}
