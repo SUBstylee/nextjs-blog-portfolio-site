27a23ebc9bd4325736516f65d3792182ee1fc7e9
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-12 21:58:50 +0800

    fixed image issues with updating next.js

--- components/blog/Author.jsx
+++ components/blog/Author.jsx
@@ -1,16 +1,25 @@
-import Image from "next/image";
+import Image from 'next/image'
 
-const Author = ({author}) => {
-    return (
-      <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
-        <div className="absolute left-0 right-0 -top-14">
-          <Image unoptimized src={author.photo.url} alt="author.name" height='100px' width='100px' className="align-middle rounded-full" />
-        </div>
-        <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
-        <p className="text-white text-lg">{author.bio}</p>
-        <a href="/portfolio" className="btn mt-5">Portfolio</a>
+const Author = ({ author }) => {
+  return (
+    <div className="relative mb-8 mt-20 rounded-lg bg-black bg-opacity-20 p-12 text-center">
+      <div className="absolute -top-14 left-0 right-0">
+        <Image
+          unoptimized
+          src={author.photo.url}
+          alt="author.name"
+          height={100}
+          width={100}
+          className="rounded-full align-middle"
+        />
       </div>
-    );
-  };
-  
-  export default Author;
+      <h3 className="my-4 text-xl font-bold text-white">{author.name}</h3>
+      <p className="text-lg text-white">{author.bio}</p>
+      <a href="/portfolio" className="btn mt-5">
+        Portfolio
+      </a>
+    </div>
+  )
+}
+
+export default Author
