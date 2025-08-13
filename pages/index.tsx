49d54bf4134d01ae15537f405b9075777a0932d6
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-13 20:13:09 +0800

    revalidate to prevent site from becoming stale

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,37 @@
-import Head from 'next/head';
-import { PostCard, Categories, PostWidget } from '../components/blog';
+import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components/blog'
 import { getPosts } from '../services'
-import { FeaturedPosts } from '../sections';
+import { FeaturedPosts } from '../sections'
 
 export default function Home({ posts }) {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto mb-8 px-10">
       <Head>
         <title>JJThrelfall Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <FeaturedPosts />
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => <PostCard post={post.node} key={index} />)}
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post, index) => (
+            <PostCard post={post.node} key={index} />
+          ))}
         </div>
-        <div className="lg:col-span-4 col-span-1">
-          <div className="lg:sticky relative top-8">
-            <PostWidget categories='' slug='' />
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
+            <PostWidget categories="" slug="" />
             <Categories />
           </div>
         </div>
       </div>
-
     </div>
-  );
-};
+  )
+}
 
 export async function getStaticProps() {
-  const posts = (await getPosts()) || [];
+  const posts = (await getPosts()) || []
   return {
-    props: { posts }
-  };
-};
+    props: { posts },
+    revalidate: 60, // regenerate the page at most every 60 seconds
+  }
+}
