a2561144abb3562b888ac66e7474537dbf4106eb
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-13 16:07:26 +0800

    rtl warning fix

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import React, { useState, useEffect } from 'react'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
 
-import { FeaturedPostCard } from '../components/blog';
-import { getFeaturedPosts } from '../services';
+import { FeaturedPostCard } from '../components/blog'
+import { getFeaturedPosts } from '../services'
 
 const responsive = {
   superLargeDesktop: {
@@ -22,54 +22,86 @@ const responsive = {
     breakpoint: { max: 640, min: 0 },
     items: 1,
   },
-};
+}
 
 const FeaturedPosts = () => {
-  const ArrowFix = (arrowProps) => { const {carouselState, children, ...restArrowProps} = arrowProps; return ( <span {...restArrowProps}> {children} </span> ); };
+  const ArrowFix = (arrowProps) => {
+    const { carouselState, children, rtl, ...restArrowProps } = arrowProps
+    return <span {...restArrowProps}> {children} </span>
+  }
 
-  const [featuredPosts, setFeaturedPosts] = useState([]);
-  const [dataLoaded, setDataLoaded] = useState(false);
+  const [featuredPosts, setFeaturedPosts] = useState([])
+  const [dataLoaded, setDataLoaded] = useState(false)
 
   useEffect(() => {
-    let abortController=new AbortController();
+    let abortController = new AbortController()
     getFeaturedPosts().then((result) => {
-      setFeaturedPosts(result);
-      setDataLoaded(true);
-    });
-    return ()=>{
-      abortController.abort();
-    };
-  },[]);
+      setFeaturedPosts(result)
+      setDataLoaded(true)
+    })
+    return () => {
+      abortController.abort()
+    }
+  }, [])
 
   const customLeftArrow = (
     <ArrowFix>
-    <div className="absolute arrow-btn left-0 text-center py-3 cursor-pointer bg-600 bg__theme border__slider rounded-full">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text__theme w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-      </svg>
-    </div>
+      <div className="arrow-btn bg-600 bg__theme border__slider absolute left-0 cursor-pointer rounded-full py-3 text-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="text__theme h-6 w-6 w-full"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M10 19l-7-7m0 0l7-7m-7 7h18"
+          />
+        </svg>
+      </div>
     </ArrowFix>
-  );
+  )
 
   const customRightArrow = (
     <ArrowFix>
-    <div className="absolute arrow-btn right-0 text-center py-3 cursor-pointer bg-600 bg__theme border__slider rounded-full">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text__theme w-full float-right" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
-      </svg>
-    </div>
+      <div className="arrow-btn bg-600 bg__theme border__slider absolute right-0 cursor-pointer rounded-full py-3 text-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="text__theme float-right h-6 w-6 w-full"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M14 5l7 7m0 0l-7 7m7-7H3"
+          />
+        </svg>
+      </div>
     </ArrowFix>
-  );
+  )
 
   return (
     <div className="mb-8">
-      <Carousel infinite customLeftArrow={customLeftArrow} customRightArrow={customRightArrow} responsive={responsive} itemClass="px-4">
-        {dataLoaded && featuredPosts.map((post, index) => (
-          <FeaturedPostCard key={index} post={post} />
-        ))}
+      <Carousel
+        infinite
+        customLeftArrow={customLeftArrow}
+        customRightArrow={customRightArrow}
+        responsive={responsive}
+        itemClass="px-4"
+      >
+        {dataLoaded &&
+          featuredPosts.map((post, index) => (
+            <FeaturedPostCard key={index} post={post} />
+          ))}
       </Carousel>
     </div>
-  );
-};
+  )
+}
 
-export default FeaturedPosts;
+export default FeaturedPosts
